ad5dc3f3ae419d1ac52ec96fd42cbac3095342ae
glenda <glenda@crack>
2026-02-25 23:09:25 +0000

    Implement plumbing to sam

--- echo.c
+++ echo.c
@@ -1,37 +1,69 @@
 #include <u.h>
 #include <libc.h>
 #include <stdio.h>
+#include <plumb.h>
 
 int
 main(int argc, char *argv[])
 {
-    int afd, lfd, dfd, n;
+    Plumbmsg plumb;
+    Plumbattr attr;
+    int pfd, afd, lfd, dfd, n;
     char adir[40], ldir[40];
-    char buf[256];
+    char buf[128];
+    char *s;
 
+    plumb.src = "echo";
+    plumb.dst = "edit";
+    plumb.wdir = "/mnt/term";
+    plumb.type = "text";
+    plumb.attr = &attr;
+    attr.name = "addr";
+    attr.value = "0";
+    attr.next = NULL;
+    
+    pfd = plumbopen("send", OWRITE);
+    if(pfd < 0){
+        perror("plumbopen");
+        return -1;
+    }
     afd = announce("udp!*!11222", adir);
     if(afd < 0)
         return -1;
     for(;;){
-        printf("listen\n");
+//        printf("listen\n");
         lfd = listen(adir, ldir);
         if(lfd < 0)
             return -1;
-        printf("fork\n");
+//        printf("fork\n");
         switch(fork()){
         case -1:
             perror("forking");
             close(lfd);
             break;
         case 0:
-            printf("accept\n");
+//            printf("accept\n");
             dfd = accept(lfd, ldir);
             if(dfd < 0)
                 return -1;
-            printf("read\n");
-            while((n = read(dfd, buf, sizeof(buf))) > 0){
-                buf[n] = '\0';
-                printf("%s\n", buf);
+//            printf("read\n");
+            
+            memset(buf, 0, sizeof(buf));
+            if((n = read(dfd, buf, sizeof(buf))) > 0){
+                //printf("read %ld bytes\n", n);
+                buf[n] = 0;
+                if((s = strchr(buf, '\n')) != nil)
+                    *s = 0;
+                s = buf;
+                if(buf[0] == '/')
+                    s++;
+                print("n: %ld: %s\n", n, s);
+                plumb.ndata = strlen(s);
+                plumb.data = s;
+                if(plumbsend(pfd, &plumb) < 0){
+                    perror("plumbsend");
+                    return -1;
+                }
             }
             exits(nil);
         default:
