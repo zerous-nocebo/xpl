a92a0e4fba6fe5e93b6a9870f575afb5b1b7c4d9
Naveen Narayanan <zerous@nocebo.space>
2026-03-06 22:16:51 +0000

    Add support for range in file

--- echo.c
+++ echo.c
@@ -11,6 +11,7 @@ main(int argc, char *argv[])
     int pfd, afd, lfd, dfd, n;
     char adir[40], ldir[40];
     char buf[128];
+    char range[16];
     char *s;
 
     plumb.src = "echo";
@@ -49,17 +50,24 @@ main(int argc, char *argv[])
 //            printf("read\n");
             
             memset(buf, 0, sizeof(buf));
+            memset(range, 0, sizeof(range));
             if((n = read(dfd, buf, sizeof(buf))) > 0){
                 //printf("read %ld bytes\n", n);
                 buf[n] = 0;
                 if((s = strchr(buf, '\n')) != nil)
                     *s = 0;
+                if((s = strchr(buf, ':')) != nil){
+                	strcpy(range, s+1);
+                	*s = 0;
+                }
                 s = buf;
                 if(buf[0] == '/')
                     s++;
+                
                 print("n: %ld: %s\n", n, s);
                 plumb.ndata = strlen(s);
                 plumb.data = s;
+                attr.value = range;
                 if(plumbsend(pfd, &plumb) < 0){
                     perror("plumbsend");
                     return -1;
